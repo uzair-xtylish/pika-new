dfc4f57267fd0017eec1f810412457c261a69898
sehar1267 <nameemoji1@gmail.com>
2025-04-24 18:01:29 +0500

    Update and rename đấm.js to Punch.js

--- modules/commands/Punch.js
+++ modules/commands/Punch.js
@@ -2,12 +2,12 @@ const request = require("request");
 const fs = require("fs")
 const axios = require("axios")
 module.exports.config = {
-  name: "đấm",
+  name: "punch",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "Kaneki",
-  description: "Đấm người bạn tag",
-  commandCategory: "Hành Động",
+  credits: "uzairrajput",
+  description: "Punch the person you tag",
+  commandCategory: "Act",
   usages: "[tag]",
   cooldowns: 5,
 };
@@ -21,7 +21,7 @@ module.exports.run = async({ api, event, Threads, global }) => {
    ];
    var mention = Object.keys(event.mentions);
      let tag = event.mentions[mention].replace("@", "");
-    if (!mention) return api.sendMessage("Vui lòng tag 1 người", threadID, messageID);
-   var callback = () => api.sendMessage({body:`${tag}` + ` 𝗕𝗲𝗺 𝘃𝗼̂ 𝗺𝗮̣̆𝘁 𝗰𝗵𝗼́ 𝗺𝗮̀𝘆 𝗻𝗲̀ 𝗰𝗵𝘂̛̀𝗮 𝘁𝗮̣̂𝘁 𝘀𝗮̂𝗻 𝘀𝗶 𝗻𝗵𝗲𝗲 👿`,mentions: [{tag: tag,id: Object.keys(event.mentions)[0]}],attachment: fs.createReadStream(__dirname + "/cache/puch.gif")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/puch.gif"));  
+    if (!mention) return api.sendMessage("𝐤𝐢𝐬𝐢 𝐛𝐡𝐢 1 𝐩𝐚𝐫𝐬𝐨𝐧 𝐤𝐨 𝐭𝐚𝐠 𝐤𝐚𝐫", threadID, messageID);
+   var callback = () => api.sendMessage({body:`${tag}` + ` 𝐏𝐢𝐤𝐚 𝐢𝐧 𝐭𝐡𝐞 𝐟𝐚𝐜𝐞 𝐰𝐚𝐢𝐭𝐢𝐧𝐠 𝐟𝐨𝐫 𝐭𝐡𝐞 𝐦𝐚𝐜𝐡𝐢𝐧𝐞 𝐭𝐨 𝐜𝐨𝐧𝐭𝐚𝐢𝐧 𝐚𝐥𝐥 𝐭𝐡𝐞 𝐩𝐫𝐨𝐝𝐮𝐜𝐭𝐬 👿`,mentions: [{tag: tag,id: Object.keys(event.mentions)[0]}],attachment: fs.createReadStream(__dirname + "/cache/puch.gif")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/puch.gif"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/puch.gif")).on("close",() => callback());
    }
