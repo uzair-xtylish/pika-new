acd5d8a851212799dfe372b2e8458549e6f46d3b
uzair1267 <0uzairrajput@gmail.com>
2025-06-04 22:26:28 +0500

    Update and rename 0_BOSSHERE.js to boss.js

---
+++ modules/commands/boss.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+
+module.exports.config = {
+
+  name: "boss",
+
+    version: "1.0.1",
+
+  hasPermssion: 0,
+
+  credits: "uzairrajput", 
+
+  description: "no prefix",
+
+  commandCategory: "No command marks needed",
+
+  usages: "...",
+
+    cooldowns: 5, 
+
+};
+
+
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+
+  var { threadID, messageID } = event;
+
+  if (event.body.indexOf("uzair")>=0 || (event.body.indexOf("Uzair")>=0 || (event.body.indexOf("Boss")>=0 || (event.body.indexOf("Boss")>=0)))) {
+
+    var msg = {
+
+        body: "🥰𝗕𝗢𝗦𝗦 𝗜𝗦 𝗛𝗘𝗥𝗘❤️\n\n● ──────────────────── ●\n\n♥️*_~اور ہمیـــّْ͢ـں ہـــّْ͢ـر دل میـــّْ͢ـں اترنـــّْ͢ـے کی چاہـــّْ͢ـت نہیـــّْ͢ـں...💸\n\n● ──────────────────── ●\n\n‎🌸===『*★𝗖𝗿𝗲𝗱𝗶𝘁'𝘀 𒁍⃝𝐔ʑʌīī𝐑┼•__🦋•",
+
+        attachment: fs.createReadStream(__dirname + `/uzair/uzair.png`)
+
+      }
+
+      api.sendMessage(msg, threadID, messageID);
+
+    }
+
+  }
+
+  module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+
+
+          }
