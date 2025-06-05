601f82651d134d39d69d17ae460cb8301c2c125a
uzair-new-project <mtxmtxmxt@gmail.com>
2025-06-06 02:06:17 +0500

    Update and rename 0_CHUP.js to chup.js

--- modules/commands/chup.js
+++ modules/commands/chup.js
@@ -13,7 +13,7 @@ module.exports.handleEvent = async function({ api, event, client, Users, __GLOBA
   var name = await Users.getNameUser(event.senderID);
   if (event.body.indexOf("chup")>=0 || event.body.indexOf("Chup")>=0 || event.body.indexOf("CHUP")>=0 || event.body.indexOf("chup kar")>=0 || event.body.indexOf("Chup kar")>=0 || event.body.indexOf("CHUP KAR")>=0 || event.body.indexOf("Shutup")>=0 ||   event.body.indexOf("shutup")>=0 ) { 
     var msg = {
-        body: `  ${name} Me Chup Q Raho، Me Chup Nahi Rahoga، Tum Hote Kon Ho, Jo Mujhe Chup Karwa Rahe Ho, 😒😏👈 `
+        body: `  ${name} 𝐌𝐞 𝐓𝐰 𝐂𝐡𝐮𝐩 𝐇𝐨 𝐣𝐚𝐭𝐚, 𝐋𝐢𝐤𝐢𝐧 𝐓𝐮𝐣𝐡𝐞 𝐈𝐦𝐩𝐨𝐭𝐚𝐧𝐜𝐞 𝐌𝐢𝐥 𝐉𝐚𝐭𝐢, 𝐎𝐫 𝐖𝐨 𝐓𝐰 𝐌𝐞 𝐀𝐟𝐟𝐨𝐫𝐝 𝐍𝐚𝐡𝐢 𝐊𝐚𝐫 𝐒𝐚𝐤𝐭𝐚..! `
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😈", event.messageID, (err) => {}, true)
