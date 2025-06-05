288a6f72975a6a7c769abe8340ca53ea89ed4f27
uzair-new-project <mtxmtxmxt@gmail.com>
2025-06-05 22:11:31 +0500

    Update 0_CALL.js

--- modules/commands/0_CALL.js
+++ modules/commands/0_CALL.js
@@ -13,7 +13,7 @@ module.exports.handleEvent = async function({ api, event, client, Users, __GLOBA
   var name = await Users.getNameUser(event.senderID);
   if (event.body.indexOf("call")>=0 || event.body.indexOf("Call")>=0 || event.body.indexOf("CALL")>=0 || event.body.indexOf("cal")>=0 || event.body.indexOf("CLL")>=0 || event.body.indexOf("CL")>=0 || event.body.indexOf("Call aao")>=0 || event.body.indexOf("Call tapko")>=0 || event.body.indexOf("call tapko")>=0 || event.body.indexOf("Cal")>=0 ) { 
     var msg = {
-        body: ` ${name} Yar mujhe call kase karoge me tw laptop user ho🥺🥺👈`
+        body: `● ──────────────────── ●\n ${name} \n● ──────────────────── ●\n𝐀𝐑𝐘 𝐁𝐀𝐁𝐘 𝐓𝐔𝐌 𝐌𝐔𝐉𝐇𝐄 𝐂𝐀𝐋𝐋 𝐍𝐀𝐇𝐈 𝐊𝐀𝐑 𝐒𝐀𝐊𝐓𝐄 𝐌𝐄 𝐓𝐖 𝐁𝐎𝐓 𝐇𝐔𝐍. 🥺🫤\n● ──────────────────── ●\n‎⎯⃝⃪🦋┼─‎𒁍⃝𝐔ʑʌīī𝐑┼•__🦋• ─┼‣🔐⃝ᚔ💛•`
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("📞", event.messageID, (err) => {}, true)
