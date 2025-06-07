a7df191d5c8b1610adace227ac9aeee17cd80a23
mtxuzairrajput <uzairrajputt1@gmail.com>
2025-06-08 04:31:58 +0500

    Update gn.js

--- modules/commands/gn.js
+++ modules/commands/gn.js
@@ -18,7 +18,7 @@ module.exports.handleEvent = async ({ api, event, Users, Currencies, args, utils
 react.includes("goodnight") ||
 react.includes("Goodnight")) {
     var msg = {
-        body: `🌙 𝐆𝐨𝐨𝐝 𝐍𝐢𝐠𝐡𝐭, 𝐁𝐚𝐛𝐲 ${name} 😴💖\n\n✨ 𝐊𝐡𝐮𝐚𝐛𝐨𝐧 𝐦𝐞 𝐦𝐮𝐬𝐤𝐮𝐫𝐚𝐧𝐚, 𝐨𝐫 𝐝𝐢𝐥 𝐦𝐞𝐢𝐧 𝐛𝐚𝐬𝐚 𝐥𝐞𝐧𝐚...\n𝐓𝐚𝐤𝐞 𝐜𝐚𝐫𝐞 𝐦𝐞𝐫𝐞 𝐉𝐚𝐚𝐧 🥰😘\n\n💤 𝐑𝐞𝐬𝐭 𝐰𝐞𝐥𝐥 𝐚𝐧𝐝 𝐬𝐥𝐞𝐞𝐩 𝐭𝐢𝐠𝐡𝐭 💫`,attachment: fs.createReadStream(__dirname + `/uzair/gnn.gif`)
+        body: `🌙 𝐆𝐨𝐨𝐝 𝐍𝐢𝐠𝐡𝐭, 𝐁𝐚𝐛𝐲 ${name} 😴💖\n\n✨ 𝐊𝐡𝐮𝐚𝐛𝐨𝐧 𝐦𝐞 𝐦𝐮𝐬𝐤𝐮𝐫𝐚𝐧𝐚, 𝐨𝐫 𝐝𝐢𝐥 𝐦𝐞𝐢𝐧 𝐛𝐚𝐬𝐚 𝐥𝐞𝐧𝐚...\n𝐓𝐚𝐤𝐞 𝐜𝐚𝐫𝐞 𝐦𝐞𝐫𝐞 𝐉𝐚𝐚𝐧 🥰😘\n\n💤 𝐑𝐞𝐬𝐭 𝐰𝐞𝐥𝐥 𝐚𝐧𝐝 𝐬𝐥𝐞𝐞𝐩 𝐭𝐢𝐠𝐡𝐭 💫\n● ──────────────────── ●\n𒁍⃝𝐌𝐀𝐃𝐄 𝐁𝐘 𝐔ʑʌīī𝐑┼•__🦋•`,attachment: fs.createReadStream(__dirname + `/uzair/gnn.gif`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🥱", event.messageID, (err) => {}, true)
