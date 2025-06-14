1de387f3273a4c9a1f35833278ba418d3e3a040e
mtxuzairrajput <uzairrajputt1@gmail.com>
2025-06-14 22:47:46 +0500

    Update and rename 0_THARKI.js to tharki.js

--- modules/commands/tharki.js
+++ modules/commands/tharki.js
@@ -13,10 +13,10 @@ module.exports.handleEvent = async function({ api, event, client, Users, __GLOBA
   var name = await Users.getNameUser(event.senderID);
   if (event.body.indexOf("Tharki")>=0 ||  event.body.indexOf("tarki")>=0 ) { 
     var msg = {
-        body: ` ${name} tum thak gaye ho, tumhra baap thaka.hua hai, tumhara pura khandan tharki teri gf bhi tharki saliya-😒😒👈`
+        body: ` ${name} \n● ──────────────────── ●\n𝑻𝒉𝒂𝒓𝒌𝒊 𝒍𝒐𝒈𝒐𝒏 𝒌𝒆 𝒍𝒊𝒚𝒆 𝒕𝒐𝒉 𝑮𝒐𝒐𝒈𝒍𝒆 𝒃𝒉𝒊 𝒘𝒂𝒓𝒏𝒊𝒏𝒈 𝒅𝒆𝒕𝒂 𝒉𝒂𝒊: 『𝐃𝐨 𝐍𝐨𝐭 𝐃𝐢𝐬𝐭𝐮𝐫𝐛 – 𝐂𝐫𝐢𝐧𝐠𝐞 𝐀𝐡𝐞𝐚𝐝!』🚧🤮`
       }
       api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("📞", event.messageID, (err) => {}, true)
+    api.setMessageReaction("😬", event.messageID, (err) => {}, true)
     }
   }
   module.exports.run = function({ api, event, client, __GLOBAL }) {
