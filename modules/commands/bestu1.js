7a817c10faebc6db84ce7e2eeaaa5774f7fe4598
innocent-uzair <uzairrajputemoji9@gmail.com>
2025-07-02 02:44:23 +0500

    Update bestu1.js

--- modules/commands/bestu1.js
+++ modules/commands/bestu1.js
@@ -50,8 +50,8 @@ async function makeImage({ one, two }) {
   const circle1 = await jimp.read(await circle(pathOne));
   const circle2 = await jimp.read(await circle(pathTwo));
 
-  bg.composite(circle1.resize(770, 770), 280, 500);
-  bg.composite(circle2.resize(730, 730), 2130, 530);
+  bg.composite(circle1.resize(770, 770), 320, 500);
+  bg.composite(circle2.resize(730, 730), 2130, 540);
 
   const buffer = await bg.getBufferAsync("image/png");
   fs.writeFileSync(pathFinal, buffer);
