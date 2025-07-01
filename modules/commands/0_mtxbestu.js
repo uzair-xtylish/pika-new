cc3933596723c5690c41c4d0fd50f72ae436bbdf
innocent-uzair <uzairrajputemoji9@gmail.com>
2025-07-01 23:50:37 +0500

    Update 0_mtxbestu.js

--- modules/commands/0_mtxbestu.js
+++ modules/commands/0_mtxbestu.js
@@ -50,8 +50,8 @@ async function makeImage({ one, two }) {
   const circle1 = await jimp.read(await circle(pathOne));
   const circle2 = await jimp.read(await circle(pathTwo));
 
-  bg.composite(circle1.resize(885, 885), 471, 731);
-  bg.composite(circle2.resize(888, 888), 1980, 734);
+  bg.composite(circle1.resize(885, 885), 461, 680);
+  bg.composite(circle2.resize(888, 888), 1980, 680);
 
   const buffer = await bg.getBufferAsync("image/png");
   fs.writeFileSync(pathFinal, buffer);
