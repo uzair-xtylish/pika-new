14eb2625770e876c36591cdfee9f01aa792c33a0
innocent-uzair <uzairrajputemoji9@gmail.com>
2025-07-01 15:38:52 +0500

    Update bestie4.js

--- modules/commands/bestie4.js
+++ modules/commands/bestie4.js
@@ -50,8 +50,8 @@ async function makeImage({ one, two }) {
   const circle1 = await jimp.read(await circle(pathOne));
   const circle2 = await jimp.read(await circle(pathTwo));
 
-  bg.composite(circle1.resize(185, 185), 65, 100);
-  bg.composite(circle2.resize(185, 185), 400, 100);
+  bg.composite(circle1.resize(185, 185), 65, 110);
+  bg.composite(circle2.resize(185, 185), 400, 110);
 
   const buffer = await bg.getBufferAsync("image/png");
   fs.writeFileSync(pathFinal, buffer);
