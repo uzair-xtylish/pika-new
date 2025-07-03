10393fbd212b97fdd26c726038114746f070b010
uzair-xtylish <zubairuzair376@gmail.com>
2025-07-03 13:44:06 +0500

    Update bro.js

--- modules/commands/bro.js
+++ modules/commands/bro.js
@@ -16,10 +16,10 @@ module.exports.config = {
 
 module.exports.onLoad = async () => {
   const dir = path.join(__dirname, "uzair", "mtx");
-  const imgPath = path.join(dir, "mtxbroo.png");
+  const imgPath = path.join(dir, "brother.png");
   if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
   if (!fs.existsSync(imgPath)) {
-    const imgData = (await axios.get("https://i.ibb.co/HLBJWPYt/mtxbroo.png", { responseType: "arraybuffer" })).data;
+    const imgData = (await axios.get("https://i.ibb.co/tM2wxFPb/brother.png", { responseType: "arraybuffer" })).data;
     fs.writeFileSync(imgPath, imgData);
   }
 };
@@ -32,8 +32,8 @@ async function circle(imagePath) {
 
 async function makeImage({ one, two }) {
   const basePath = path.join(__dirname, "uzair", "mtx");
-  const bg = await jimp.read(path.join(basePath, "mtxbroo.png"));
-  const pathFinal = path.join(basePath, `bro_${one}_${two}.png`);
+  const bg = await jimp.read(path.join(basePath, "brother.png"));
+  const pathFinal = path.join(basePath, `brother_${one}_${two}.png`);
   const pathOne = path.join(basePath, `avt_${one}.png`);
   const pathTwo = path.join(basePath, `avt_${two}.png`);
 
